dc934d530c9c7827bd318358db251891a64c68a5
Leon Bowie <leon@bowie-co.nz>
2021-03-23 14:02:43 +1300

    fix typo

--- obsidion/core/events.py
+++ obsidion/core/events.py
@@ -35,7 +35,6 @@ class Events(commands.Cog):
             perms.send_messages = True
             perms.read_messages = True
             perms.embed_links = True
-            perms.
             url = discord.utils.oauth_url(
                 self.bot.user.id,
                 permissions=perms,
