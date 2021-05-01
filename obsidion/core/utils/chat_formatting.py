6c4c42c29134e1e3242e6375b3df3fab17e75d13
Leon Bowie <leon@bowie-co.nz>
2021-05-01 17:24:55 +1200

    Fix mypy

--- obsidion/core/utils/chat_formatting.py
+++ obsidion/core/utils/chat_formatting.py
@@ -473,7 +473,7 @@ def format_perms_list(perms: discord.Permissions) -> str:
 def humanize_timedelta(
     *,
     timedelta: Optional[datetime.timedelta] = None,
-    seconds: Optional[int] = None,
+    seconds: Optional[SupportsInt] = None,
 ) -> str:
     """
     Get a locale aware human timedelta representation.
@@ -487,7 +487,7 @@ def humanize_timedelta(
     ----------
     timedelta: Optional[datetime.timedelta]
         A timedelta object
-    seconds: Optional[int]
+    seconds: Optional[SupportsInt]
         A number of seconds
 
     Returns
@@ -501,9 +501,12 @@ def humanize_timedelta(
         The function was called with neither a number of seconds nor a timedelta object
     """
 
-    if timedelta is None and seconds is None:
+    if seconds is not None:
+        obj = seconds
+    elif timedelta is not None:
+        obj = timedelta.total_seconds()
+    else:
         raise ValueError("You must provide either a timedelta or a number of seconds")
-    obj = seconds if seconds is not None else timedelta.total_seconds()
 
     seconds = int(obj)
     periods = [
