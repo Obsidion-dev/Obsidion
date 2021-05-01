01046c398913a21bc8fe48f19db3f5dad0bfa255
Leon Bowie <leon@bowie-co.nz>
2021-05-01 17:09:24 +1200

    Update password management

--- migrations/env.py
+++ migrations/env.py
@@ -3,6 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,14 +26,15 @@ def run_migrations_offline():
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
+    here as well. By skipping the Engine creation
     we don't even need a DBAPI to be available.
 
     Calls to context.execute() here emit the given string to the
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.environ["DB"]
+    print(url)
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -51,8 +53,10 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
+    _config = config.get_section(config.config_ini_section)
+    _config["sqlalchemy.url"] = os.environ["DB"]
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        _config,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
